65708817f7896c460bf274b6c9ebd262173f0e46
Mthobisi-za <mthobisimtho333@gmail.com>
2021-11-03 08:40:35 +0200

    commas

--- the-balloon.sql
+++ the-balloon.sql
@@ -1,10 +1,10 @@
 create table valid_color (id serial not null primary key, color_name text, count integer);
 create table invalid_color (id serial not null primary key, color_name text, count integer);
 
-insert into invalid_color(color_name, count)values('Blue',0)
-insert into invalid_color(color_name, count)values('Red',0)
-insert into invalid_color(color_name, count)values('Green',0)
+insert into invalid_color(color_name, count)values('Blue',0);
+insert into invalid_color(color_name, count)values('Red',0);
+insert into invalid_color(color_name, count)values('Green',0);
 
-insert into valid_color(color_name, count)values('Lime',0)
-insert into valid_color(color_name, count)values('Purple',0)
-insert into valid_color(color_name, count)values('Orange',0)
+insert into valid_color(color_name, count)values('Lime',0);
+insert into valid_color(color_name, count)values('Purple',0);
+insert into valid_color(color_name, count)values('Orange',0);
